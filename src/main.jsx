16e7c2469ed970e47eaa2b25f99ff2ae69613d1d
xoargadaob <nozadzek1@gmail.com>
2026-05-13 02:19:46 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 
-const API_URL = 'https://rameiqneba.onrender.com/api/thoughts/match';
+const API_URL = 'https://rameiqneba.onrender.com/';
 
 function App() {
   const [text, setText] = useState('');
